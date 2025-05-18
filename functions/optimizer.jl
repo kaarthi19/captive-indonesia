f69bbf7844bbe6d2e73a6f372834b1edd3fc8a48
Kaarthi A Gnapathy <63862946+kaarthi19@users.noreply.github.com>
2025-05-18 00:04:12 -0700

    Update optimizer.jl

--- functions/optimizer.jl
+++ functions/optimizer.jl
@@ -307,7 +307,7 @@ function capacity_expansion(inputs, mipgap, CO2_constraint, CO2_limit, RE_constr
                 sum(vIP_GEN[t,g] for g in intersect(inputs.ip_generators[inputs.ip_generators.Industrial_Park.==ip,:R_ID],inputs.IP_RE)) +
                 sum(vIP_IMPORT[t,ip]) +
                 sum(vIP_NSE[t,s,ip] for s in inputs.IP_S) - 
-                sum(vCHARGE[t,g] for g in intersect(inputs.generators[inputs.ip_generators.Industrial_Park.==ip,:R_ID],inputs.IP_STOR)) -
+                sum(vCHARGE[t,g] for g in intersect(inputs.ip_generators[inputs.ip_generators.Industrial_Park.==ip,:R_ID],inputs.IP_STOR)) -
                 # include export variable here -
                 inputs.ip_demand[t,ip] == 0
             end);
